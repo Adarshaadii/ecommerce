2ce36f82ce2e74d8ac9351804a306a71cc06ba2f
Adarshaadii <113962989+Adarshaadii@users.noreply.github.com>
2022-09-30 15:49:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,23 +17,6 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
 
-// Route Imports
-const product = require("./routes/productRoute");
-const user = require("./routes/userRoute");
-const order = require("./routes/orderRoute");
-const payment = require("./routes/paymentRoute");
-
-app.use("/api/v1", product);
-app.use("/api/v1", user);
-app.use("/api/v1", order);
-app.use("/api/v1", payment);
-
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-});
-
 // Middleware for Errors
 app.use(errorMiddleware);
 
